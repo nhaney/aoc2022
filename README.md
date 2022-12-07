be1866e84824b0d5bf4d017f645487d7c0ca6f16
Nigel Haney <nigel.haney27@gmail.com>
2022-12-06 23:50:45 -0800

    complete day 6

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Goal is to finish this for the first time this year, so I am going to be using p
 
 For an extra challenge, I am going to try to code in a functional style which means I am going to ban the following from my solutions (more to come possibly as I proceed):
 
-    * For loops
-    * Mutable state
+* For loops (not counting list/dict comprehension)
+* Mutable state
---
+++ day6/day6.py
@@ -0,0 +1,49 @@
+import os
+from dataclasses import dataclass
+from itertools import accumulate
+from typing import Dict, Iterator, Self
+
+
+@dataclass
+class DistinctWindow:
+    distinct_chars: Dict[str, int]
+    min_index: int
+    max_index: int
+
+    @classmethod
+    def init_from_iter(cls, it: Iterator[str]) -> Self:
+        first_char = next(it)
+        assert len(first_char) == 1
+        return cls({first_char: 0}, 0, 0)
+
+    @property
+    def window_length(self) -> int:
+        return (self.max_index - self.min_index) + 1
+
+
+def update_window(cur: DistinctWindow, next_char: str) -> DistinctWindow:
+    assert len(next_char) == 1
+    repeat_index = cur.distinct_chars.get(next_char, -1)
+    new_distinct_chars = {c: i for c, i in cur.distinct_chars.items() if i > repeat_index}
+
+    this_index = cur.max_index + 1
+    new_distinct_chars[next_char] = this_index
+    new_min_index = repeat_index + 1 if repeat_index != -1 else cur.min_index
+
+    return DistinctWindow(new_distinct_chars, new_min_index, this_index)
+
+
+def find_first_index_after_distinct_char_marker(stream: Iterator[str], marker_length: int) -> int:
+    windows = accumulate(stream, update_window, initial=DistinctWindow.init_from_iter(stream))
+    return next(filter(lambda x: x.window_length >= marker_length, windows)).max_index + 1
+
+
+with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as f:
+    text = f.read()
+
+
+# part 1
+print(find_first_index_after_distinct_char_marker(iter(text), 4))
+
+# part 2
+print(find_first_index_after_distinct_char_marker(iter(text), 14))
